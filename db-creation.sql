c94fc4f1331e0cadc12a40ea09a583a43bb5d676
Daniela Ortega1 <danielasuarezb.03@gmail.com>
2024-09-05 23:54:06 -0500

    Feat: Upload MySql Script to create de database

--- db-creation.sql
+++ db-creation.sql
@@ -1,26 +1,90 @@
--- Script para crear la base de datos y las tablas necesarias
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-09-2024 a las 06:07:22
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
-CREATE DATABASE IF NOT EXISTS college_manager;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-USE professor_assistant_db;
 
--- Tabla para almacenar la información del curso
-CREATE TABLE IF NOT EXISTS courses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    cut1_percentage FLOAT NOT NULL,
-    cut2_percentage FLOAT NOT NULL,
-    cut3_percentage FLOAT NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Tabla para almacenar la lista de estudiantes
-CREATE TABLE IF NOT EXISTS students (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    code VARCHAR(255) NOT NULL,
-    full_name VARCHAR(255) NOT NULL,
-    emails VARCHAR(255) NOT NULL,
-    course_id INT,
-    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
-);
+--
+-- Base de datos: `telepizza`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `IdCliente` int(11) NOT NULL,
+  `NombreCompleto` varchar(30) NOT NULL,
+  `Sexo` varchar(20) NOT NULL,
+  `Edad` int(10) NOT NULL,
+  `Ciudad` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pqrs`
+--
+
+CREATE TABLE `pqrs` (
+  `Idpqrs` int(11) NOT NULL,
+  `Tipo` varchar(30) NOT NULL,
+  `ClienteFk` int(20) NOT NULL,
+  `FechaCaso` date NOT NULL,
+  `Asunto` varchar(30) NOT NULL,
+  `Estado` varchar(30) NOT NULL,
+  `FechaCierre` date NOT NULL,
+  `Urgencia` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`IdCliente`);
+
+--
+-- Indices de la tabla `pqrs`
+--
+ALTER TABLE `pqrs`
+  ADD PRIMARY KEY (`Idpqrs`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `IdCliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT de la tabla `pqrs`
+--
+ALTER TABLE `pqrs`
+  MODIFY `Idpqrs` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
